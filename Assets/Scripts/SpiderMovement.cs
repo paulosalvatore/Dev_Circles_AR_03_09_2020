4a931d4d623f9b28b4ad9b8dbb989b559e39a3f2
Paulo Salvatore <paulo@salvatore.tech>
2020-09-03 20:48:01 -0300

    Spider Movement

---
+++ Assets/Scripts/SpiderMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderMovement : MonoBehaviour
+{
+    public float speed = 0.2f;
+
+    public float rotationSpeed = 5f;
+
+    public Vector3 targetPosition;
+
+    public Animator anim;
+
+    private void Update()
+    {
+        var initialPosition = transform.position;
+
+        var finalPosition = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        transform.position = finalPosition;
+
+        var direction = finalPosition - initialPosition;
+
+        var distance = Vector3.Distance(finalPosition, initialPosition);
+
+        if (distance > 0 && direction != Vector3.zero)
+        {
+            var targetRotation = Quaternion.LookRotation(direction);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
+    }
+}
